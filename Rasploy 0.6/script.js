cc6366f9ad25e432e05983d77262c33697b07a12
walidayankawsar <walidayankawsar@gmail.com>
2025-06-11 13:40:59 +0600

    Add files via upload

---
+++ Rasploy 0.6/script.js
@@ -0,0 +1,15 @@
+	function toggleDarkMode() {
+	    document.body.classList.toggle("dark-mode");
+
+	    if (document.body.classList.contains("dark-mode")) {
+	        localStorage.setItem("theme", "dark");
+	    } else {
+	        localStorage.setItem("theme", "light");
+	    }
+	}
+
+	window.onload = function () {
+	    if (localStorage.getItem("theme") === "dark") {
+	        document.body.classList.add("dark-mode");
+	    }
+	}
